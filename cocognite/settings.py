26d8dfbdce0a0af8047c872bc9a339ed6931f5c6
daavethedev <Umair_mnz105@yahoo.com>
2020-10-10 03:48:50 +0500

    Allowed all hosts

--- cocognite/settings.py
+++ cocognite/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'cocognite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'charset': 'utf8mb4'},
+        'NAME': 'cocognitodb',
+        'USER': 'umair',
+        'PASSWORD': 'multi-mediaplus123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
