c25bed46b0a35f4e24116c45b51846596653fd82
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2020-10-11 00:12:43 +0500

    Prototype 10% Completed

--- application/views.py
+++ application/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 
 def home(request):
     return render(request=request, template_name='home.html')
+
+
+def signup(request):
+    return render(request=request, template_name='signup.html')
+
+
+def login(request):
+    return render(request=request, template_name='login.html')
+
+
+def parent_login(request):
+    return render(request=request, template_name='parents_login.html')
+
+
+def profile_update(request):
+    return render(request=request, template_name='profile_update.html')
+
