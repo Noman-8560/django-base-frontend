9a570780bc734319faaaf2d2a4fc19a299efa6f7
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2020-10-11 04:29:05 +0500

    Multi Screen Pages added, News Panel Added and spellings problem cleared

--- application/views.py
+++ application/views.py
@@ -5,6 +5,10 @@ def home(request):
     return render(request=request, template_name='home.html')
 
 
+def help_view(request):
+    return render(request=request, template_name='help.html')
+
+
 def signup(request):
     return render(request=request, template_name='signup.html')
 
@@ -19,4 +23,17 @@ def parent_login(request):
 
 def profile_update(request):
     return render(request=request, template_name='profile_update.html')
+
+
+def quiz_user_1(request):
+    return render(request=request, template_name='quiz_user_1.html')
+
+
+def quiz_user_2(request):
+    return render(request=request, template_name='quiz_user_2.html')
+
+
+def quiz_user_3(request):
+    return render(request=request, template_name='quiz_user_3.html')
+
 
