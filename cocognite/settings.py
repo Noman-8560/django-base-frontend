e07054e7da744d39ed4d07ceb7b7c7f0e1ecc531
daavethedev <Umair_mnz105@yahoo.com>
2020-10-22 13:03:50 +0500

    Settings updated.

--- cocognite/settings.py
+++ cocognite/settings.py
@@ -76,24 +76,25 @@ WSGI_APPLICATION = 'cocognite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {'charset': 'utf8mb4'},
-        'NAME': 'cocognitodb',
-        'USER': 'umair',
-        'PASSWORD': 'multi-mediaplus123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+if os.name == 'nt':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+elif os.name == 'posix':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'OPTIONS': {'charset': 'utf8mb4'},
+            'NAME': 'cocognitodb',
+            'USER': 'umair',
+            'PASSWORD': 'multi-mediaplus123',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
