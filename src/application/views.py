3b8e0bbb609bf3be0c03df8c6e94f09861c1fbd5
daavethedev <Umair_mnz105@yahoo.com>
2021-05-09 23:55:25 +0500

    Zoom updated.

--- src/application/views.py
+++ src/application/views.py
@@ -1080,7 +1080,7 @@ def enroll(request, pk):
 
         if int(quiz.players) > 1:
             response = zoom_create_meeting(name=f"QUIZ {quiz.title} - TEAM {team_name}",
-                                           start_time=str(quiz.start_time), host="cocognito20@gmail.com")
+                                           start_time=str(quiz.start_time), host=request.user.email)
             if response.status_code != 201:
                 messages.error(request=request, message=f'Failed To create zoom meeting please consult administration')
                 return HttpResponseRedirect(reverse('application:enroll_quiz', args=(quiz.pk,)))
