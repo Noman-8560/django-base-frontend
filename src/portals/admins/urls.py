4c96033902a120da9ac46e9ec55b8ef7babb191f
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2021-11-03 20:28:25 +0500

    ADMINS: subjects and blogs rebuild > completed > 8%

--- src/portals/admins/urls.py
+++ src/portals/admins/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path, include
 
 from src.portals.admins.views import (
-    DashboardView,
+    DashboardView, ArticleListView, ArticleDeleteView, ArticleCreateView, ArticleUpdateView, ArticleDetailView,
+    SubjectListView, SubjectDetailView, SubjectCreateView, SubjectUpdateView, SubjectDeleteView
 )
 
 app_name = "admin-portal"
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard')
+    path('', DashboardView.as_view(), name='dashboard'),
+
+    path('article/', ArticleListView.as_view(), name='article'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('add/article/', ArticleCreateView.as_view(), name='article-create'),
+    path('update/article/<int:pk>/', ArticleUpdateView.as_view(), name='article-update'),
+    path('delete/article/<int:pk>/', ArticleDeleteView.as_view(), name='article-delete'),
+
+    path('subject/', SubjectListView.as_view(), name='subject'),
+    path('subject/<int:pk>/', SubjectDetailView.as_view(), name='subject-detail'),
+    path('add/subject/', SubjectCreateView.as_view(), name='subject-create'),
+    path('update/subject/<int:pk>/', SubjectUpdateView.as_view(), name='subject-update'),
+    path('delete/subject/<int:pk>/', SubjectDeleteView.as_view(), name='subject-delete'),
+
 ]
--- src/portals/admins/views.py
+++ src/portals/admins/views.py
@@ -1,4 +1,10 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    TemplateView, ListView, DeleteView, DetailView, UpdateView, CreateView
+)
+from src.application.models import (
+    Article, Subject,
+)
 
 
 class DashboardView(TemplateView):
@@ -7,4 +13,80 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         return context
+
+
+"""  ARTICLES --------------------------------------------------------------------- """
+
+
+class ArticleListView(ListView):
+    models = Article
+    queryset = Article.objects.all()
+    template_name = 'admins/article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    models = Article
+    queryset = Article.objects.all()
+    template_name = 'admins/article_detail.html'
+
+
+class ArticleCreateView(CreateView):
+    models = Article
+    queryset = Article.objects.all()
+    fields = '__all__'
+    template_name = 'admins/article_create_form.html'
+    success_url = reverse_lazy('admin-portal:article')
+
+
+class ArticleUpdateView(UpdateView):
+    models = Article
+    queryset = Article.objects.all()
+    fields = '__all__'
+    template_name = 'admins/article_create_form.html'
+    success_url = reverse_lazy('admin-portal:article')
+
+
+class ArticleDeleteView(DeleteView):
+    models = Article
+    queryset = Article.objects.all()
+    template_name = 'admins/article_delete.html'
+    success_url = reverse_lazy('admin-portal:article')
+
+
+"""  SUBJECTS --------------------------------------------------------------------- """
+
+
+class SubjectListView(ListView):
+    models = Subject
+    queryset = Subject.objects.all()
+    template_name = 'admins/subject_list.html'
+
+
+class SubjectDetailView(DetailView):
+    models = Subject
+    queryset = Subject.objects.all()
+    template_name = 'admins/subject_detail.html'
+
+
+class SubjectCreateView(CreateView):
+    models = Subject
+    queryset = Subject.objects.all()
+    fields = '__all__'
+    template_name = 'admins/subject_create_form.html'
+    success_url = reverse_lazy('admin-portal:subject')
+
+
+class SubjectUpdateView(UpdateView):
+    models = Subject
+    queryset = Subject.objects.all()
+    fields = '__all__'
+    template_name = 'admins/subject_create_form.html'
+    success_url = reverse_lazy('admin-portal:subject')
+
+
+class SubjectDeleteView(DeleteView):
+    models = Subject
+    queryset = Subject.objects.all()
+    template_name = 'admins/subject_delete.html'
+    success_url = reverse_lazy('admin-portal:subject')
 
