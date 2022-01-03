51249e779de7881647c7eb8ef90ab42004ba4795
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-01-03 13:09:33 +0500

    WEBSITE: website previous logic removed

--- src/wsite/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from  .models import Website
-
-
-class WebsiteForm(ModelForm):
-    class Meta:
-        model = Website
-        fields = '__all__'
--- src/wsite/urls.py
+++ src/wsite/urls.py
@@ -1,13 +1,2 @@
 from django.urls import path
-from .views import event, article, articles, site_builder, home, page_404, page_500, coming_soon
 
-app_name = 'wsite'
-urlpatterns = [
-    path('event/<slug:slug>/', event, name='event'),
-    path('article/<slug:slug>/', article, name='article'),
-    path('articles/', articles, name='articles'),
-    path('builder/', site_builder, name='site_builder'),
-    path('coming_soon/', coming_soon, name='coming_soon'),
-    path('404/', page_404, name='404'),
-    path('500/', page_500, name='500'),
-]
