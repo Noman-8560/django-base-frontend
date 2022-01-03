6630e78df5b079fe317c57592087d39f2956f196
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-01-03 13:13:53 +0500

    WEBSITE: previous fully removed

--- cocognite/urls.py
+++ cocognite/urls.py
@@ -4,18 +4,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from src.wsite.views import (
-    home, page_404, page_500, coming_soon
-)
 from . import settings
 
 urlpatterns = [
 
     # BASE URLS -------------------------------------------------------- #
-    path('', home, name='home'),
-    path('404/', coming_soon, name='404'),
-    path('500/', coming_soon, name='500'),
-    path('coming-soon/', coming_soon, name='coming_soon'),
 
     # REQUIRED --------------------------------------------------------- #
     path('admin/', admin.site.urls),
@@ -23,14 +16,12 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
     # PORTALS ---------------------------------------------------------- #
-    # path('', include('src.wsite.urls', namespace='wsite')),
     path('a/', include('src.portals.admins.urls', namespace='admin-portal')),
     path('s/', include('src.portals.student.urls', namespace='student-portal')),
     path('m/', include('src.portals.moderator.urls', namespace='moderator-portal')),
     path('p/', include('src.portals.parent.urls', namespace='parent-portal')),
 
     # DEPRECATED ------------------------------------------------------- #
-    path('site/', include('src.wsite.urls', namespace='wsite')),
     path('', include('src.application.urls', namespace='application')),
     path('zoom/', include('src.zoom_api.urls', namespace='zoom_api')),
 
