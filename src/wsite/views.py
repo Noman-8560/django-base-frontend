3f519dd9b74f9fc362fb247f6ac0ad0d72edfd10
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-01-07 07:03:05 +0500

    WEBSITE: home page backend implemented

--- src/wsite/views.py
+++ src/wsite/views.py
@@ -1,8 +1,16 @@
 from django.views.generic import TemplateView
 
+from src.application.models import Quiz
+
 
 class HomeView(TemplateView):
     template_name = 'wsite/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['quizzes'] = Quiz.objects.filter(visible_on_home=True, learning_purpose=False)
+        context['learning'] = Quiz.objects.filter(visible_on_home=True, learning_purpose=True)
+        return context
 
 
 class Error404View(TemplateView):
