cea7f4c381d5a2a280429038c2468b6ccef364a3
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-01-09 20:39:14 +0500

    WEBSITE: header donw

--- src/wsite/urls.py
+++ src/wsite/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    HomeView, Error404View, LearningListView, QuizListView
+    HomeView, Error404View, LearningListView, QuizListView, PrivacyPolicyView, TermsAndConditionsView
 )
 
 app_name = "wsite"
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('quizzes/', QuizListView.as_view(), name='quizzes'),
     path('learning-resources/', LearningListView.as_view(), name='learning-resources'),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
+    path('terms-and-conditions/', TermsAndConditionsView.as_view(), name='terms-and-conditions'),
     path('404/', Error404View.as_view(), name='404'),
 ]
