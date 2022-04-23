5947a29743dea0a563c5bc6c9473840b0d38143b
jhonydev <star.jk901@gmail.com>
2022-04-23 10:15:12 +0500

    testing db auto switching on local environments implemented

--- cocognite/settings.py
+++ cocognite/settings.py
@@ -8,11 +8,15 @@ AUTH_USER_MODEL = 'accounts.User'
 ROOT_URLCONF = 'cocognite.urls'
 WSGI_APPLICATION = 'cocognite.wsgi.application'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+env_file = os.path.join(BASE_DIR, ".env")
+env = os.environ.Env()
+env.read_env(env_file)
 
 SECRET_KEY = 's(l5vi&5nq3619gdskadhgjaksd981234hlaskhjdlasd'
 ZOOM_API_KEY_JWT = 'EBB0k1HnRN6hlD5dvrkAyw'
 ZOOM_API_SECRET_JWT = '1hnrKhnDfgbZDsg5WdLKxEIA9bZsPBm2BKOF'
 
+USE_TESTING_DB_SERVER = env('USE_TEST_DB_SERVER') == 'True'
 DEBUG = True
 SERVER = False
 TEST = False
@@ -138,12 +142,20 @@ if SERVER:
             }
         }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    if USE_TESTING_DB_SERVER:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': '/home/umair/db.sqlite3',
+            }
         }
-    }
+    else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
 
 """ INTERNATIONALIZATION ----------------------------------------------------------------------------------------- """
 
@@ -188,7 +200,6 @@ ACCOUNT_USERNAME_REQUIRED = False
 OLD_PASSWORD_FIELD_ENABLED = True
 LOGOUT_ON_PASSWORD_CHANGE = False
 ACCOUNT_EMAIL_VERIFICATION = 'none'
-
 
 """ RESIZER IMAGE --------------------------------------------------------------------------------"""
 DJANGORESIZED_DEFAULT_SIZE = [1920, 1080]
